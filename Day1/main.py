40bf0da105988daf841dd0ee400bfcb2d6598ad6
Arjun Bhardwaj <aarjunbhardwaj@gmail.com>
2024-08-16 21:18:59 +0530

    Manage Data With Variables

--- Day1/main.py
+++ Day1/main.py
@@ -1,1 +1,5 @@
 print("Welcome to The Band Name Generator!")
+city = input("What city did you grow up in?\n")
+animal =input("what is your favoraite animal\n")
+print(f"Your Band Name is {city} {animal}")
+             
